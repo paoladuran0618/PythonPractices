8e8071e521768e05651b4a849d5f4e8d9baabb1f
paola duran <duranpao0618@gmail.com>
2021-01-07 07:45:48 -0400

    Dibujo de un cuadrado usando turtle

---
+++ platzi/dibujar.py
@@ -0,0 +1,16 @@
+import turtle # Para hacer graficos
+
+window = turtle.Screen()
+david = turtle.Turtle()
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+
+turtle.mainloop()
+
+
