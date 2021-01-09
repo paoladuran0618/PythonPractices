20c42be5e1d0db5cd77205a1aadf05a866118fed
paola duran <duranpao0618@gmail.com>
2021-01-09 16:32:30 -0400

    Algunos codigos

---
+++ platzi/string.py
@@ -0,0 +1,18 @@
+string = 'paola'
+
+for letter in string:
+    print(letter)
+
+mi_string = 'Leonardo Medina'
+print(len(mi_string))
+
+string_2 = 'Eso es otro string super largo'
+print(string_2.upper())
+print(string_2.lower())
+print(string_2.find('s'))
+
+print(string_2[1:])
+print(string[1:3])
+print(string_2[1:6:2])
+print(string_2[::-1]) #Lee al reves la palabra
+#print(string_2[start:ends:stepts])
