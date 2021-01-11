12775d8d6a1d6ce950d30166974d2ca57d1d239c
paola duran <duranpao0618@gmail.com>
2021-01-11 17:14:19 -0400

    Un programa que te dice si una palabra es polindromo

---
+++ platzi/polindromo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def run():
+    word = input('Introduce la palabra: ')
+    if word == word[::-1]:
+        print("La palabra es un políndromo.")
+    else:
+        print('No es un polindromo')
+
+if __name__ == '__main__':
+    run()
